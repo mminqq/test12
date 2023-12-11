9a5d8e13771442be1258c4cf10a7e34fe1d7c600
ParkDongR <144304929+ParkDongR@users.noreply.github.com>
2023-12-12 00:20:29 +0900

    Delete lib/screens/web_screen directory

--- lib/screens/web_screen/bus.dart
+++
@@ -1,8 +0,0 @@
-class Bus {
-  final String busName;
-  final String busId;
-  final List<List<bool>> seatStatus;
-
-  Bus({required this.busName, required this.busId})
-      : seatStatus = List.generate(2, (row) => List.filled(5, false));
-}
